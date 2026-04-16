ab0be2671daa06b068e95029562fd1a8544fdacc
iljakolesnikov2001-rgb <ilja.kolesnikov2001@gmail.com>
2026-04-16 18:45:50 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.querySelector(".login-form");
+
+  if (!form) return;
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+  });
+});
